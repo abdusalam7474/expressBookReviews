5c85d9531a628d18fc52526bae0a3eb5e41f8ce9
abdusalam7474 <mideola824@gmail.com>
2024-08-07 17:37:08 +0100

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -5,13 +5,33 @@ const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
 const app = express();
-
+const secret = "123456";
 app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
 //Write the authenication mechanism here
+    //login yet?
+    let tkn = req.header('Authorization');
+    if(!tkn){
+        return res.status(401).json({"message":"No token found. You need to login with your username and password "})
+    }
+
+    if(tkn.startsWith('Bearer ')){ //yes loggedin
+        let tokenValue = tkn.slice(7, tkn.length).trimLeft();
+        let verificationobj = jwt.verify(tokenvalue, secret);
+        if(verificationobj.user=="user"){
+            console.log("verification Successful");
+            next();            
+        }else{ //No not login
+            console.log("verification Not Successful");
+            return res.status(401).json({"message":"Access Denied: you need to login with your username and password "})
+        }
+    }else{ //No not login
+        console.log("verification Not Successful");
+        return res.status(401).json({"message":"Access Denied: you need to login with your username and password "})
+    }
 });
  
 const PORT =5000;
